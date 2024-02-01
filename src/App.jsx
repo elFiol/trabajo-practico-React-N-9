298771c20c1990c0d3ff5348c48e7171fd0ef3b8
Fiol Thiago <fiolthiago9@gmail.com>
2024-02-01 19:56:41 -0300

    añadimos el react boostrap y añadimos algo de maquetado en el formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import './App.css'
+import { Container } from 'react-bootstrap'
+import "bootstrap/dist/css/bootstrap.min.css"
+import FormularioDoc from './components/FormularioDoc'
 
 function App() {
   return (
-    <>
-    </>
+    <Container className='my-5'>
+      <h2 className='text-center'>Administrador pacientes veterinaria</h2>
+      <FormularioDoc></FormularioDoc>
+    </Container>
   )
 }
 
--- src/components/FormularioDoc.jsx
+++ src/components/FormularioDoc.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import "../App.css"
+import { Button, Container, Form } from 'react-bootstrap';
 
 const FormularioDoc = () => {
     return (
         <div>
-            
+            <div>
+                <p>Llenar el formulario para crear cita</p>
+            </div>
+            <div className='docForm px-2 '>
+                <Form>
+                    <Container>
+                        <Form.Group className='d-lg-flex d-md-flex justify-content-between my-3'>
+                            <Form.Label>Nombre de la mascota:</Form.Label>
+                            <Form.Control type='text' placeholder='nombre de la mascota' className='w-md-75 w-lg-75'></Form.Control>
+                        </Form.Group>
+                        <Form.Group className='d-lg-flex d-md-flex justify-content-between my-3'>
+                            <Form.Label>Nombre de la mascota:</Form.Label>
+                            <Form.Control type='text' placeholder='nombre de la mascota' className='w-md-75 w-lg-75'></Form.Control>
+                        </Form.Group>
+                        <Form.Group className='d-lg-flex d-md-flex justify-content-between'>
+                            <div className='fechaYhora d-lg-flex d-md-flex justify-content-between'>
+                                <Form.Label className="fechaYhoraLabel">Nombre de la mascota:</Form.Label>
+                                <Form.Control type='text' placeholder='nombre de la mascota' className='w-md-75 w-lg-75'></Form.Control>
+                            </div>
+                            <div className='fechaYhora d-lg-flex d-md-flex justify-content-between'>
+                                <Form.Label className="fechaYhoraLabel">Nombre de la mascota:</Form.Label>
+                                <Form.Control type='text' placeholder='nombre de la mascota' className='w-md-75 w-lg-75'></Form.Control>
+                            </div>
+                        </Form.Group>
+                    </Container>
+                    <div>
+                        <Button>Agregar nueva cita</Button>
+                    </div>
+                </Form>
+            </div>
         </div>
     );
 };
